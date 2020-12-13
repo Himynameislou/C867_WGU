e832fdc48568b0e1b3818ee032d9488b78837c69
Luis Vegerano <louvwebdev@gmail.com>
2020-12-13 15:22:30 -0600

    Initial Commit

---
+++ C867/student.cpp
@@ -0,0 +1,8 @@
+//
+//  student.cpp
+//  C867
+//
+//  Created by Luis Vegerano on 12/13/20.
+//
+
+#include "student.hpp"
---
+++ C867/student.hpp
@@ -0,0 +1,13 @@
+//
+//  student.hpp
+//  C867
+//
+//  Created by Luis Vegerano on 12/13/20.
+//
+
+#ifndef student_hpp
+#define student_hpp
+
+#include <stdio.h>
+
+#endif /* student_hpp */
