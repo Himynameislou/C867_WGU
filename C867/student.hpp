d0ecd017b654eef253ad7ccfd16a221f5c3e4ee8
Luis Vegerano <louvwebdev@gmail.com>
2020-12-13 16:13:11 -0600

    Building Student Class: -Student ID -first name -last name -email -age -number of days in course (array) -degree program

--- C867/student.hpp
+++ C867/student.hpp
@@ -9,5 +9,18 @@
 #define student_hpp
 
 #include <stdio.h>
+// Include CString Library
+#include <string>
+// Include Degree Header File
+#include "./degree.h"
+
+using namespace std;
+
+
+class Student {
+public:
+    Student(); //Default Constructor
+    Student(string studentID, string firstName, string lastName, string email, int age, int* daysInCourse, DegreeProgram);
+};
 
 #endif /* student_hpp */
