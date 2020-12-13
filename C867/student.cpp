dbe741476f841ac285bbc561b23a74f95ea9b06e
Luis Vegerano <louvwebdev@gmail.com>
2020-12-13 17:49:14 -0600

    Defining getters - Get Degree function

--- C867/student.cpp
+++ C867/student.cpp
@@ -25,3 +25,48 @@ Student::Student(string studenID, string fName, string lName, string email, int 
     numberDaysPerClass[1] = daysCourse[1];
     numberDaysPerClass[2] = daysCourse[2];
 }
+
+//Getters
+string Student::getStudentId(){
+    return studentID;
+}
+string Student::getFirstName(){
+    return firstName;
+}
+string Student::getLastName(){
+    return lastName;
+}
+string Student::getEmail(){
+    return email;
+}
+int Student::getAge(){
+    return age;
+}
+int *Student::getNumberDays(){
+    return numberDaysPerClass;
+}
+void Student::getDegreeProg(){
+    return DegreeProgram::SOFTWARE;
+}
+
+//Setters
+void Student::setStudentId(string studenID){
+    Student::studentID = studenID;
+}
+void Student::setFirstName(string fName){
+    Student::firstName = fName;
+}
+void Student::setLastName(string lName){
+    Student::lastName = lName;
+}
+void Student::setEmail(string email){
+    Student::email = email;
+}
+void Student::setAge(int age){
+    Student::age = age;
+}
+void Student::setDaysCourse(int d0, int d1, int d2){
+    Student::numberDaysPerClass[0] = d0;
+    Student::numberDaysPerClass[1] = d1;
+    Student::numberDaysPerClass[2] = d2;
+}
