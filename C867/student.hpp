a79f5df15b92c69627d664e299523cbbe8bb1d6d
Luis Vegerano <louvwebdev@gmail.com>
2020-12-13 16:40:05 -0600

    Declared class function and variables

--- C867/student.hpp
+++ C867/student.hpp
@@ -20,7 +20,27 @@ using namespace std;
 class Student {
 public:
     Student(); //Default Constructor
+    
+    //Student Array
     Student(string studentID, string firstName, string lastName, string email, int age, int* daysInCourse, DegreeProgram);
+    
+    
+    //Getters
+    string getStudentId();
+    string getFirstName();
+    string getLastName();
+    string getEmail();
+    int getAge();
+    int *getNumberDays();
+    
+    //Setters
+    void setStudentId(string studentID);
+    void setFirstName(string firstName);
+    void setLastName(string lastName);
+    void setEmail(string email);
+    void setAge(int age);
+    void setDaysCourse(int* daysInCourse);
+    void setDegreeProgram(DegreeProgram);
 };
 
 #endif /* student_hpp */
