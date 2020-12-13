6fcf830e4f03bf108a8ec9bc24f77ab7f60fb821
Luis Vegerano <louvwebdev@gmail.com>
2020-12-13 17:05:15 -0600

    Removed DegreeProgram from Student array

--- C867/student.cpp
+++ C867/student.cpp
@@ -6,3 +6,15 @@
 //
 
 #include "student.hpp"
+#include <iostream>
+#include "./degree.h"
+#include <string>
+
+using namespace std;
+
+Student::Student(){
+}
+
+Student::Student(string studenID, string fName, string lName, string email, int age, int* daysCourse){
+    
+}
