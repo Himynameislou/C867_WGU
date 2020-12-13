d5eeb873d2a8a1addfda05dae03c2a4d0414eeef
Luis Vegerano <louvwebdev@gmail.com>
2020-12-13 15:39:27 -0600

    INSTRUCTION - complete "C. Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE."

---
+++ C867/degree.h
@@ -0,0 +1,17 @@
+//
+//  degree.h
+//  C867
+//
+//  Created by Luis Vegerano on 12/13/20.
+//
+
+#ifndef degree_h
+#define degree_h
+
+// Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.
+enum DegreeProgram {
+    SECURITY, NETWORK, SOFTWARE
+}
+
+
+#endif /* degree_h */
