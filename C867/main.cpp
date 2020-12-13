96f96d28eee4c821af9af30d9623cb39456de99d
Luis Vegerano <louvwebdev@gmail.com>
2020-12-13 15:18:23 -0600

    Initial Commit

---
+++ C867/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C867
+//
+//  Created by Luis Vegerano on 12/13/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
