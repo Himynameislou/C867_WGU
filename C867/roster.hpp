bd90b75f9ce0c8420fdc4f5e2e622d0f703186e1
Luis Vegerano <louvwebdev@gmail.com>
2020-12-14 16:57:46 -0600

    Continued Roster Decleration

--- C867/roster.hpp
+++ C867/roster.hpp
@@ -16,5 +16,18 @@ class Roster {
 public:
     Roster(); //Default constructor for Roster
     
-}
+    // Setting Instance Variables from part D1
+    void add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degree);
+    
+    void removeStudent(string studentID);
+    void printCourseDays(string studentID);
+    void printInvalidEmails();
+    void printDegreeType();
+    
+private:
+    //Array for Roster
+    Student *ClassRosterArray[5];
+    
+    
+};
 #endif /* roster_hpp */
