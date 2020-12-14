ff979308d169a51ba6f8ae4810f5cf1aafb280a3
Luis Vegerano <louvwebdev@gmail.com>
2020-12-14 15:27:26 -0600

    Added Roster Class file and header. Began building class declerations

---
+++ C867/roster.cpp
@@ -0,0 +1,8 @@
+//
+//  roster.cpp
+//  C867
+//
+//  Created by Luis Vegerano on 12/14/20.
+//
+
+#include "roster.hpp"
---
+++ C867/roster.hpp
@@ -0,0 +1,20 @@
+//
+//  roster.hpp
+//  C867
+//
+//  Created by Luis Vegerano on 12/14/20.
+//
+
+#ifndef roster_hpp
+#define roster_hpp
+#include <array>
+#include <stdio.h>
+#include "./student.hpp"
+using namespace std;
+
+class Roster {
+public:
+    Roster(); //Default constructor for Roster
+    
+}
+#endif /* roster_hpp */
