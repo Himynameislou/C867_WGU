6fa6576f5a2522e98cf7ee830c2d3dd9d3ea3c32
Luis Vegerano <louvwebdev@gmail.com>
2020-12-17 16:43:26 -0600

    Added a string that uses the degree types as indexes

--- C867/degree.h
+++ C867/degree.h
@@ -13,7 +13,10 @@ using namespace std;
 
 // Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.
 
-enum class DegreeProgram {SECURITY, NETWORK, SOFTWARE};
+enum DegreeProgram {SECURITY, NETWORK, SOFTWARE};
+
+//Using degree type as index
+static const string degreeString[]= {"SECURITY", "NETWORK", "SOFTWARE"};
 
 
 #endif /* degree_h */
