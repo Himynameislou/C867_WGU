0849a4a5ef070730a833d3fa8715f91c8cb8eeb5
Luis Vegerano <louvwebdev@gmail.com>
2020-12-18 14:32:08 -0600

    Added student string

--- C867/main.cpp
+++ C867/main.cpp
@@ -13,9 +13,13 @@
 using namespace std;
 
 int main() {
-    
-    Student stud1;
-    Roster ros1;
-    ros1.pringAll();
-    
+    //String to Parse
+    const string studentData[] =
+    {
+        "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+        "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+        "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+        "A5,Luis,Vegerano,louie1598@gmail.com,37,33, 20,32,SOFTWARE"
+    };
 };
