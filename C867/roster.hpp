ce208800b4c1848d319a22c4d75818048e398936
Luis Vegerano <louvwebdev@gmail.com>
2020-12-18 14:32:45 -0600

    Refactored code; improved methods.

--- C867/roster.hpp
+++ C867/roster.hpp
@@ -10,31 +10,39 @@
 #include <array>
 #include <stdio.h>
 #include "student.hpp"
-#include "degree.h"
+
 using namespace std;
 
 class Roster {
-public:
-    Roster(); //Default constructor for Roster
-    
-    // Setting Instance Variables from part D1
-    void add(string studentID, string firstName, string lastName, string emailAddress, int age, int dCourse1, int dCourse2, int dCourse3, DegreeProgram degreeProgram);
-    void removeStudent(string studentID);
-    void pringAll();
-    void printAverageDaysInCourse(string studentID);
-    void printInvalidEmails();
-    void printByDegreeProgram(DegreeProgram degreeProgram);
-    
-    //adding index
-    int addingIndex;
-    
-    ~Roster();
     
 private:
+    int lastEntry = -1; //Used to loop over STUDENTS when adding to Roster
     //Array for Roster
-    Student *ClassRosterArray[5];
+    static const int numStudents = 5;
+    Student* ClassRosterArray[numStudents];
     
+public:
+    Roster(); //Default constructor for Roster
+    void parser(string row); //Will be used to parse data properly into row
     
-    
+    // Setting Instance Variables from part D1
+    //Adding student data into Roster
+    void add(
+             string sID,
+             string fN,
+             string lN,
+             string eAddress,
+             int age,
+             int dCourse1,
+             int dCourse2,
+             int dCourse3,
+             DegreeProgram degPro);
+    void pringAll();
+    void printByDegreeProgram(DegreeProgram degPro);
+    void printInvalidEmails();
+    void printAverageDaysInCourse(string studentID);
+    void removeStudent(string sID);
+
+    ~Roster();
 };
 #endif /* roster_hpp */
